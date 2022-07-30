c702a188fc34b6761656747010042b263d8a8c9e
Marcin Sztylko <marcin.sztylko@gmail.com>
2022-07-30 19:30:26 +0200

    add working version of Scheme calculator

---
+++ calc.scm
@@ -0,0 +1,35 @@
+(define (calc)
+  (display "calc: ")
+  ;;(flush)
+  (print (calc-eval (read)))
+  (newline)
+  (calc))
+
+;;(define (accumulate combiner null-value term a next b)
+;;  (if (> a b)
+;;      null-value
+;;      (combiner (term a)
+;;                (accumulate combiner null-value term (next a) next b))))
+
+(define (accumulate op initial items)
+    (if (null? items)
+        initial
+        (op (car items)
+            (accumulate op initial (cdr items)))))
+    
+
+(define (calc-eval exp)
+  (cond ((number? exp) exp)
+        ((list? exp) (calc-apply (car exp) (map calc-eval (cdr exp))))
+        (else (error "Calc: bad expression:" exp))))
+
+(define (calc-apply fn args)
+  (cond ((eq? fn '+) (accumulate + 0 args))
+        ((eq? fn '-) (cond ((null? args) (error "Calc: no args to -"))
+                           ((= (length args) 1) (- (car args)))
+                           (else (- (car args) (accumulate + 0 (cdr args))))))
+        ((eq? fn '*) (accumulate * 1 args))
+        ((eq? fn '/) (cond ((null? args) (error "Calc: no args to /"))
+                           ((= (length args) 1) (/ (car args)))
+                           (else (/ (car args) (accumulate * 1 (cdr args))))))
+        (else (error "Calc: bad operator:" fn))))
